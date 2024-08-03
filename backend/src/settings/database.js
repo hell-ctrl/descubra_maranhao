6b3bd9a4bad3b12e2f594e8d0f23543af018b5fa
hell-ctrl <reidogado499@gmail.com>
2024-08-03 16:44:45 -0300

    update database settings

--- backend/src/settings/database.js
+++ backend/src/settings/database.js
@@ -1,8 +1,10 @@
 const dotenv = require("dotenv").config();
 
 module.exports = {
-  dialect: 'mariadb',
+  dialect: 'mysql',
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
   username: process.env.DB_USERNAME,
   database: process.env.DB_NAME,
   define: {
