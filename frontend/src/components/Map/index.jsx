3edb94722bd4e264f5aad6353e5dfd5b141d0654
m._neto <89698038+hell-ctrl@users.noreply.github.com>
2024-08-07 16:19:14 -0300

    Update index.jsx

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -1,7 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
+
+// Definindo explicitamente o ícone do marcador
+const defaultIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  shadowSize: [41, 41],
+});
 
 function Map({ position, name }) {
   return (
@@ -10,11 +20,13 @@ function Map({ position, name }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup><a href={`https://maps.google.com/maps?q=${position[0]},${position[1]}`}>{name}</a></Popup>
+      <Marker position={position} icon={defaultIcon}>
+        <Popup>
+          <a href={`https://maps.google.com/maps?q=${position[0]},${position[1]}`}>{name}</a>
+        </Popup>
       </Marker>
     </MapContainer>
-  )
+  );
 }
 
 export default Map;
